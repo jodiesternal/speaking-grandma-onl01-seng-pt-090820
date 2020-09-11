f5340ba222c0c035c25b9e5a7b4120b851ad9df9
global-listener-7039 <jodiesternal@yahoo.com>
2020-09-11 13:17:17 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,9 +3,6 @@
 # IF YOUR YELLING AT GRANDMA, SHE WILL HEAR YOU
 # SHE SCREAMS BACK
 
-# If you shout, she can hear you (or at least she thinks so)
-# and yells back
-
 # NO, NOT SINCE 1938!
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
