53325b52989775ea696bca8a1b91df95bd19150a
global-listener-7039 <jodiesternal@yahoo.com>
2020-09-11 14:28:59 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,7 @@ def speak_to_grandma(phrase)
 
 if phrase == "I LOVE YOU GRANDMA!"
    "I LOVE YOU TOO PUMPKIN!"
-elsif phrase == "cant hear"
+elsif phrase !=phrase.upcase
    "HUH?! SPEAK UP, SONNY!"
 else "i love you grandma."
   puts "i love you to pumpkinn"
