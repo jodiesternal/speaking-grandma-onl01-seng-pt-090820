0084ad98505d2e4806d0c683ff55c472cca1bc24
global-listener-7039 <jodiesternal@yahoo.com>
2020-09-11 13:58:32 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -4,8 +4,6 @@ if grandma == "hear"
   puts "will shout back."
 elsif grandma == "cant hear"
   puts "i cant hear."
-else
-  puts "i love you grandma."
-elsif "i love you to pumpkinn"
-
+else "i love you grandma."
+  puts "i love you to pumpkinn"
 end
