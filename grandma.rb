8038994cfcc2f38d5c2567ef512c44029baa54c9
global-listener-7039 <jodiesternal@yahoo.com>
2020-09-11 13:13:34 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 # Write a speak_to_grandma method.
 
 # IF you are yelling a Grandma, she is going to hear you
-# HUH?! SPEAK UP, SONNY!
+# she screams back
 # unless you shout it (type in all capitals).
 
 # If you shout, she can hear you (or at least she thinks so)
