cfbfc4c59f10295738eae6703a1939c2ebd566a0
global-listener-7039 <jodiesternal@yahoo.com>
2020-09-11 14:21:15 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 def speak_to_grandma(phrase)
 
-if phrase == "hear"
-  puts "will shout back."
+if phrase == "I LOVE YOU GRANDMA!"
+   "will shout back."
 elsif phrase == "cant hear"
   puts "i cant hear."
 else "i love you grandma."
