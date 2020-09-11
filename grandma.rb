a25ee481532469c17d313f5324c7eea51ba7ebfb
global-listener-7039 <jodiesternal@yahoo.com>
2020-09-11 13:33:11 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,3 @@
-IF YOUR YELLING AT GRANDMA, SHE WILL HEAR YOU (OR SHE THINKS SHE CAN)
-SHE SCREAMS BACK
-if you speak to grandma, she cant hear you (or she thinks she can)
-she yells back
-
-NO, NOT SINCE 1938!
-
-However if you say 'I LOVE YOU GRANDMA!', she should respond with
-'I LOVE YOU TOO PUMPKIN!'
+if grandma = hear
+  puts grandma can hear you if you scream
+end
